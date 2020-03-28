8a54e6fa38945df1c1719d65de63767f206a358e
Nhan Doan <nhandev88@gmail.com>
2020-03-28 16:29:02 +0700

    Upgrade project
    Generated using latest `create-react-app`.

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux';
+import * as serviceWorker from './serviceWorker';
 
 import Home from './components/Home';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Home />
-  </Provider>,
+  <StrictMode>
+    <Provider store={store}>
+      <Home />
+    </Provider>
+  </StrictMode>,
   document.getElementById('root'),
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
