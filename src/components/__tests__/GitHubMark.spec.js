8ff101fe10b04ce415c6d6e5394caf23a2e0e2b7
Nhan Doan <nhandev88@gmail.com>
2020-04-11 04:27:46 +0700

    Update test suites

--- src/components/__tests__/GitHubMark.spec.js
+++ src/components/__tests__/GitHubMark.spec.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+
 import GitHubMark from '../GitHubMark';
 
 const rootUrl = 'http://localhost/';
 
-it('renders correctly', () => {
-  const { container } = render(<GitHubMark />);
-  expect(container).toMatchSnapshot();
-  expect(container.getElementsByTagName('img')[0].src)
-    .toBe(`${rootUrl}github-mark-light.svg`);
-});
+describe('<GitHubMark />', () => {
+  it('renders correctly', () => {
+    const { container } = render(<GitHubMark />);
+    expect(container).toMatchSnapshot();
+    expect(container.getElementsByTagName('img')[0].src)
+      .toBe(`${rootUrl}github-mark-light.svg`);
+  });
 
-it('toggles style', () => {
-  const { container } = render(<GitHubMark toggle />);
-  expect(container.getElementsByTagName('img')[0].src)
-    .toBe(`${rootUrl}github-mark.svg`);
+  it('toggles style', () => {
+    const { container } = render(<GitHubMark toggle />);
+    expect(container.getElementsByTagName('img')[0].src)
+      .toBe(`${rootUrl}github-mark.svg`);
+  });
 });
--- src/components/__tests__/Loading.spec.js
+++ src/components/__tests__/Loading.spec.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+
 import Loading from '../Loading';
 
-it('renders correctly', () => {
-  const { asFragment } = render(<Loading />);
-  expect(asFragment()).toMatchSnapshot();
-});
+describe('<Loading />', () => {
+  it('renders correctly', () => {
+    const { asFragment } = render(<Loading />);
+    expect(asFragment()).toMatchSnapshot();
+  });
 
-it('renders error state', () => {
-  const { container } = render(<Loading error />);
-  expect(container.firstChild.classList.contains('error')).toBe(true);
+  it('renders error state', () => {
+    const { container } = render(<Loading error />);
+    expect(container.firstChild.classList.contains('error')).toBe(true);
+  });
 });
